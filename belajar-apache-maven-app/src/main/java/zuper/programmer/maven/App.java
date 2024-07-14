4c45e393fa259ba1b29ad9320ffaed6dc396fc04
ZuperProgrammer <sirajul.umam6@gmail.com>
2024-07-14 13:16:10 +0700

    belajar apache maven

---
+++ belajar-apache-maven-app/src/main/java/zuper/programmer/maven/App.java
@@ -0,0 +1,21 @@
+package zuper.programmer.maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+
+        Person person = new Person("Mohammad Sirajul Umam");
+
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
---
+++ belajar-apache-maven-data/src/main/java/zuper/programmer/maven/Person.java
@@ -0,0 +1,17 @@
+package zuper.programmer.maven;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
